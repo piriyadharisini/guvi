05efb99330f5336dc1039d022b66c3eefdd4bbb6
piriyadharisini <31730822+piriyadharisini@users.noreply.github.com>
2017-09-15 10:01:32 +0530

    Add files via upload

--- palin.c
+++ palin.c
@@ -1,45 +1,38 @@
-bool isPalindrome(struct Node *head)
+#include <stdio.h>
+
+int main()
 {
-    struct Node *slow_ptr = head, *fast_ptr = head;
-    struct Node *second_half, *prev_of_slow_ptr = head;
-    struct Node *midnode = NULL; 
-    bool res = true;
- 
-    if (head!=NULL && head->next!=NULL)
-    {
-        
-        while (fast_ptr != NULL && fast_ptr->next != NULL)
-        {
-            fast_ptr = fast_ptr->next->next;
- 
-            prev_of_slow_ptr = slow_ptr;
-            slow_ptr = slow_ptr->next;
+    char str[100];
+    scanf("%s",str);
+    int j=strlen(str)-1,i,flag=0,ch;
+    for(i=0;i<=j;i++){
+        if(str[i]!=str[j]){
+            if(str[i]==str[j-1]){
+                ch=str[j];
+                flag++;
+            }
+            else if(str[j]==str[i+1]){
+                ch=str[i];
+                flag++;
+            }
+            else{
+                if(i+1==j-1){
+                    flag++;
+                }
+                ch=str[j];
+                flag++;
+            }
         }
- 
- 
-        
-        if (fast_ptr != NULL)
-        {
-            midnode = slow_ptr;
-            slow_ptr = slow_ptr->next;
-        }
- 
-        
-        second_half = slow_ptr;
-        prev_of_slow_ptr->next = NULL; 
-        reverse(&second_half);  
-        res = compareLists(head, second_half); 
- 
-        
-         reverse(&second_half); 
- 
-          
-         if (midnode != NULL) 
-         {
-            prev_of_slow_ptr->next = midnode;
-            midnode->next = second_half;
-         }
-         else  prev_of_slow_ptr->next = second_half;
+        j--;
     }
-    return res;
+    if(flag==1){
+        printf("%c\n",ch);
+    }
+    else if(flag>1){
+        printf("cannot be converted\n");
+    }
+    else{
+        printf("Already a palindrome\n");
+    }
+    return 0;
 }
